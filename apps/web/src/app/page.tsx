941fa424c278e06fb0dc64af58bf78ecb395fe49
Konstantinos Despoinidis <kdesp2003@gmail.com>
2025-09-21 02:11:37 +0300

    chore: added ideas for home page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -7,5 +7,13 @@ export default function Home() {
   const user = useAuth(true);
   if (!user) return <Loading />;
 
-  return <h1>Logged in as {user.username} ({user.userId})</h1>;
+  return (<>
+    <h1>Logged in as {user.username} ({user.userId})</h1>
+    Ideas for Home page:<br/>
+      - App Name / Logo<br />
+      - User Status<br />
+      - Online Friend Peers<br />
+      - Number of new messages<br />
+      - Number of pending invites<br />
+  </>);
 }
