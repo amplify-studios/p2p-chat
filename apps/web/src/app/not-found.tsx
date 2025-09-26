f651f4a29a4b5a5fec5a70def7e148b9ab62db6f
Konstantinos Despoinidis <kdesp2003@gmail.com>
2025-09-26 16:04:30 +0300

    feat: added 404 page

---
+++ apps/web/src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen text-center">
+      <h1 className="text-6xl font-bold">404</h1>
+      <p className="text-xl mt-4">Sorry, we couldn’t find that page.</p>
+      <Link href="/" className="mt-6 text-blue-600 underline">
+        Go back home
+      </Link>
+    </div>
+  );
+}
+
+
