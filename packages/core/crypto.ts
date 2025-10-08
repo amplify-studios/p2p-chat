d0005d68438b970235e7e335ac6d46cca687705f
Konstantinos Mokas <kostasmokas12@gmail.com>
2025-10-08 20:55:13 +0300

    fix: temp remove of argon

--- packages/core/crypto.ts
+++ packages/core/crypto.ts
@@ -1,23 +1,23 @@
-export interface EncryptedMessageType extends Omit<MessageType, 'roomId' | 'senderId' | 'message'> {
-  roomId: EncryptedField;
-  senderId: EncryptedField;
-  message: EncryptedField;
-}
+// export interface EncryptedMessageType extends Omit<MessageType, 'roomId' | 'senderId' | 'message'> {
+//   roomId: EncryptedField;
+//   senderId: EncryptedField;
+//   message: EncryptedField;
+// }
 
-export function encryptMessageType(msg: MessageType, key: Uint8Array): EncryptedMessageType {
-  return {
-    ...msg,
-    roomId: encField(key, msg.roomId),
-    senderId: encField(key, msg.senderId),
-    message: encField(key, msg.message),
-  };
-}
+// export function encryptMessageType(msg: MessageType, key: Uint8Array): EncryptedMessageType {
+//   return {
+//     ...msg,
+//     roomId: encField(key, msg.roomId),
+//     senderId: encField(key, msg.senderId),
+//     message: encField(key, msg.message),
+//   };
+// }
 
-export function decryptMessageType(enc: EncryptedMessageType, key: Uint8Array): MessageType {
-  return {
-    ...enc,
-    roomId: decField(key, enc.roomId),
-    senderId: decField(key, enc.senderId),
-    message: decField(key, enc.message),
-  };
-}
+// export function decryptMessageType(enc: EncryptedMessageType, key: Uint8Array): MessageType {
+//   return {
+//     ...enc,
+//     roomId: decField(key, enc.roomId),
+//     senderId: decField(key, enc.senderId),
+//     message: decField(key, enc.message),
+//   };
+// }
