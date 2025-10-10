2ccbe73621fe4c957b311d26dba347b4e3037ef2
Konstantinos Despoinidis <kdesp2003@gmail.com>
2025-10-10 22:28:13 +0300

    fix: building

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,15 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: [
-    "192.168.1.8"
+    "192.168.1.8",
+    "192.168.1.6"
   ],
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
-
-module.exports = {
-  allowedDevOrigins: ['192.168.1.6'],
-}
 
 export default nextConfig;
--- apps/web/src/app/api/subscribe/route.ts
+++
@@ -1,9 +0,0 @@
-// let subscriptions: PushSubscription[] = [];
-
-// export async function POST(req: Request) {
-//     const subscription = await req.json();
-//     subscriptions.push(subscription);
-//     return Response.json({ ok: true });
-// }
-
-// export { subscriptions };
